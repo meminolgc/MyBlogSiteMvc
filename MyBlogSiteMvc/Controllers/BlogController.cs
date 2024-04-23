1fbaa6da72a8a7091ad5ccc334713b52d6fa9dc9
meminolgc <meminolgc@gmail.com>
2024-04-23 16:16:45 +0300

    Bloh işlemleri güncelleme

--- MyBlogSiteMvc/Controllers/BlogController.cs
+++ MyBlogSiteMvc/Controllers/BlogController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,8 @@ namespace MyBlogSiteMvc.Controllers
     public class BlogController : Controller
     {
         // GET: Blog
+        BlogManager bm = new BlogManager();
+
         public ActionResult Index()
         {
             return View();
@@ -16,7 +19,8 @@ namespace MyBlogSiteMvc.Controllers
 
         public PartialViewResult BlogList()
         {
-            return PartialView();
+            var blogList = bm.GetAll();
+            return PartialView(blogList);
         }
 
         public PartialViewResult FeaturedPosts()
