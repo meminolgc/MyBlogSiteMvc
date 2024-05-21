32777df8f54ffeb33c7678dc3b6f65e896d8fb9a
meminolgc <meminolgc@gmail.com>
2024-05-21 21:27:59 +0300

    yorumlar son

--- BusinessLayer/Concrete/CommentManager.cs
+++ BusinessLayer/Concrete/CommentManager.cs
@@ -22,6 +22,16 @@ namespace BusinessLayer.Concrete
             return repoComment.List(x => x.BlogID == id);
         }
 
+        public List<Comment> CommentByStatusTrue()
+        {
+            return repoComment.List(x => x.CommentStatus == true);
+        }
+
+        public List<Comment> CommentByStatusFalse()
+        {
+            return repoComment.List(x => x.CommentStatus == false);
+        }
+
         public int CommentAdd(Comment c)
         {
             if (c.CommentText.Length <= 4 || c.CommentText.Length >= 301 || c.UserName == "" || c.Mail == "" || c.UserName.Length <= 4)
@@ -29,6 +39,20 @@ namespace BusinessLayer.Concrete
                 return -1;
             }
             return repoComment.Insert(c);
+        }
+
+        public int CommentStatusChangeToFalse(int id)
+        {
+            Comment comment = repoComment.Find(x => x.CommentID == id);
+            comment.CommentStatus = false;
+            return repoComment.Update(comment);
+        }
+
+        public int CommentStatusChangeToTrue(int id)
+        {
+            Comment comment = repoComment.Find(x => x.CommentID == id);
+            comment.CommentStatus = true;
+            return repoComment.Update(comment);
         }
 
     }
